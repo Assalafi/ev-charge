53ed485634f108cdf99b6c8c390cf17e74672e93
unknown <abubakarafamai@gmail.com>
2026-05-10 20:57:04 +0100

    Fix QR scanner for web using jsQR, upgrade mobile_scanner to v4, use HTML renderer for better performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:mobile_scanner/mobile_scanner_web_plugin.dart'
+    if (dart.library.io) 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:mobile_scanner/src/web/jsqr.dart'
+    if (dart.library.io) 'package:mobile_scanner/mobile_scanner.dart';
 import 'config/theme.dart';
 import 'providers/auth_provider.dart';
 import 'providers/theme_provider.dart';
@@ -7,6 +12,10 @@ import 'screens/splash_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    MobileScannerWebPlugin.barCodeReader =
+        JsQrCodeReader(videoContainer: MobileScannerWebPlugin.vidDiv);
+  }
   runApp(const EVChargeApp());
 }
 
